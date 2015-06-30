c5b9ac8b1e6df415915676dc87b82ef015c5328b
martinlj10 <martinlj10@hotmail.com>
2015-06-29 21:21:51 -0300

    Servlets faltantes para Baja y Modificacion de Articulos sin crear Vistas.

---
+++ src/servlets/ServletBajaArticulo.java
@@ -0,0 +1,54 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import negocio.ControladorArticulo;;
+
+/**
+ * Servlet implementation class ServletBajaArticulo
+ */
+@WebServlet("/ServletBajaArticulo")
+
+public class ServletBajaArticulo extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+    
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletBajaArticulo() {
+      
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession session=request.getSession();
+		
+		ControladorArticulo controlador = (ControladorArticulo)session.getAttribute("controladorArticulo");
+		int vIdArticulo = Integer.parseInt(request.getParameter("idArticulo"));
+		controlador.DeleteArticulo(vIdArticulo);
+		response.sendRedirect("/ListaArticulos.jsp");/*Vista no creada*/
+		
+		
+		// TODO Auto-generated method stub
+	}
+	
+}
---
+++ src/servlets/ServletModificArticulo.java
@@ -0,0 +1,64 @@
+package servlets;
+
+import java.io.IOException;
+import java.sql.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import negocio.ControladorArticulo;
+
+/**
+ * ESTE ES EL QUE MODIFICA
+ * Servlet implementation class ServletModifArticulo
+ */
+@WebServlet("/ServletModificArticulo")
+public class ServletModificArticulo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletModificArticulo() {
+        
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session=request.getSession();
+		ControladorArticulo controladorArticulo =(ControladorArticulo)session.getAttribute("controladorArticulo");
+		
+		int pID=Integer.parseInt(request.getParameter("cod_articulo"));
+		String pDescripcion =request.getParameter("descripcion").toUpperCase();
+		Date pFechaDesde = java.sql.Date.valueOf((request.getParameter("fechaDesde_precio")));
+		float pPrecio =Float.parseFloat(request.getParameter("precio"));
+		int pStock = Integer.parseInt(request.getParameter("stock"));
+		
+		
+		try{
+			controladorArticulo.UpdateArticulo(pID, pDescripcion, pStock, pFechaDesde, pPrecio);
+		response.sendRedirect("/ModificacionArticuloExitosa.jsp"); //Vista no Creada
+		}
+		catch(Exception e)
+		{
+			response.sendRedirect("/ErrorModificacionArticulo.html"); //Vista no Creada.
+		}
+		// TODO Auto-generated method stub
+	}
+
+
+}
