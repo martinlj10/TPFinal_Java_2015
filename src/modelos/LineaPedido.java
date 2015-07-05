2fec72499830ff43ebba22553973c97d7476fac5
martinlj10 <martinlj10@hotmail.com>
2015-07-05 15:57:41 -0300

    Se agregaron Nro de Pedido correspondiente a la Linea Pedido Se agrego el Nro de Linea para poder identificarlo

--- src/modelos/LineaPedido.java
+++ src/modelos/LineaPedido.java
@@ -21,6 +21,21 @@ public class LineaPedido {
 	public void setSub_total(float sub_total) {
 		this.sub_total = sub_total;
 	}
+		
+	public int getNroPedido() {
+		return nroPedido;
+	}
+	public void setNroPedido(int nroPedido) {
+		this.nroPedido = nroPedido;
+	}
+	public int getNroLinea() {
+		return nroLinea;
+	}
+	public void setNroLinea(int nroLinea) {
+		this.nroLinea = nroLinea;
+	}
+	private int nroLinea;
+	private int nroPedido;
 	private Articulo art;
 	private int cantidad;
 	private float sub_total;
