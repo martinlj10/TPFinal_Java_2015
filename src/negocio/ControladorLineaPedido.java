b3b8392e3f4c0ce8f5275626dc32c91ef9dfca51
martinlj10 <martinlj10@hotmail.com>
2015-07-05 17:29:07 -0300

    Se agregaron los controladores de Pedido y Linea de Pedido Falta Revisar como manejar las estructuras de Datos de Cliente y Articulo dentro de las anteriores estructuras respectivamente

---
+++ src/negocio/ControladorLineaPedido.java
@@ -0,0 +1,61 @@
+package negocio;
+
+import java.util.ArrayList;
+
+import modelos.*;
+import datos.*;
+
+import java.sql.*;
+
+public class ControladorLineaPedido {
+
+		private CatalogoLineaPedido catLineaPedido = new CatalogoLineaPedido();
+		
+		public int AddLineaPedido(LineaPedido nuevaLineaPedido)
+		{
+			//Validaciones de Negocio
+			
+			int idLineaPedidoNueva = catLineaPedido.AddLineaPedido(nuevaLineaPedido);
+			
+			return idLineaPedidoNueva;
+		}
+
+		public void DeleteLineaPedido(int pIdLineaPedido, int pIdPedido)
+		{
+			
+			catLineaPedido.DeleteLineaPedido(pIdLineaPedido, pIdLineaPedido);
+			
+		}
+
+		public LineaPedido getOne(int pIdLineaPedido, int pIdPedido)
+		{
+			LineaPedido LineaPedidoDev = new LineaPedido();/*Crea la estructura del LineaPedido a devolver*/
+			LineaPedidoDev = catLineaPedido.GetOne(pIdLineaPedido, pIdPedido);/*Obtiene la linea Pedido de un Pedido a devolver por IdLinea e IdPedido*/
+			
+			return LineaPedidoDev;
+		}
+		
+		public int UpdateLineaPedido(int pIdLinea, int pIdPedido, int pIdArticulo, int pCantidad, float pSubtotal)
+		{
+			int rowAffect;
+			LineaPedido modifLineaPedido = new LineaPedido();
+			modifLineaPedido.setNroPedido(pIdPedido);
+			//modifLineaPedido.setArt; ver como setear articulo
+			modifLineaPedido.setNroLinea(pIdLinea);
+			modifLineaPedido.setCantidad(pCantidad);
+			modifLineaPedido.setSub_total(pSubtotal);
+			rowAffect = catLineaPedido.UpdateLineaPedido(modifLineaPedido);
+			return rowAffect;/*Devuelve la cantidad de filas afectadas por el update*/
+			
+		}
+		
+		public ArrayList<LineaPedido> getAll(int pIdPedido)
+		{
+			ArrayList<LineaPedido> LineaPedidoAll = new ArrayList<LineaPedido>();
+			LineaPedidoAll = catLineaPedido.GetAll(pIdPedido);
+			
+			return LineaPedidoAll;
+			
+		}
+	
+}
