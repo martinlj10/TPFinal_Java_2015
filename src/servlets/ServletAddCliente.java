43982a71626d07cb151a5a9e866250a1d0b25815
martinlj10 <martinlj10@hotmail.com>
2015-07-06 19:55:30 -0300

    Agregado de ServletsAdd Cliente y Pedido

---
+++ src/servlets/ServletAddCliente.java
@@ -0,0 +1,65 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import modelos.Articulo;
+import modelos.Cliente;
+import negocio.ControladorArticulo;
+import negocio.ControladorCliente;
+
+@WebServlet("/ServletAddCliente")
+
+public class ServletAddCliente {
+
+private static final long serialVersionUID = 1L;
+    
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletAddCliente() {
+        
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session=request.getSession();
+		ControladorCliente controladorCliente =(ControladorCliente)session.getAttribute("controladorCliente");
+		Cliente nuevoCliente = new Cliente();
+		nuevoCliente.setAlias(request.getParameter("alias").toUpperCase());
+		nuevoCliente.setId_cliente(Integer.parseInt(request.getParameter("id_cliente")));
+		nuevoCliente.setId_localidad(Integer.parseInt(request.getParameter("id_localidad")));
+		nuevoCliente.setLocalidad(request.getParameter("localidad").toUpperCase());//ver si ponemos localidad o id_localidad
+		nuevoCliente.setRazon_social(request.getParameter("razon_social").toUpperCase());
+		nuevoCliente.setZona(request.getParameter("zona").toUpperCase());
+				
+		try{
+		//controladorCliente.(nuevoCliente);
+		response.sendRedirect("/ListaArticulos.jsp");/*Redirige a la lista completa de articulos*/
+		}
+		catch(Exception e)
+		{
+			response.sendRedirect("/ErrorAlta.html"); //No está creada esta vista1.
+		}
+		// TODO Auto-generated method stub
+	}
+
+	
+}
---
+++ src/servlets/ServletAddPedido.java
@@ -0,0 +1,64 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import modelos.Pedido;
+import negocio.ControladorPedido;
+
+@WebServlet("/ServletAddPedido")
+
+public class ServletAddPedido extends HttpServlet {
+
+private static final long serialVersionUID = 1L;
+    
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletAddPedido() {
+        
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session=request.getSession();
+		ControladorPedido controladorPedido =(ControladorPedido)session.getAttribute("controladorPedido");
+		Pedido nuevoPedido = new Pedido();
+		nuevoPedido.setNroPedido(Integer.parseInt(request.getParameter("id_pedido")));
+		nuevoPedido.setFechaEntrega(java.sql.Date.valueOf((request.getParameter("fecha_entrega"))));
+		nuevoPedido.setFechaPedido(java.sql.Date.valueOf((request.getParameter("fecha_pedido"))));
+		nuevoPedido.setIVA(Float.parseFloat(request.getParameter("IVA")));
+		nuevoPedido.setEstado(request.getParameter("estado"));
+		//nuevoPedido.setDetalles(detalles); ver como recuperar la lista de lineas pedidos
+		//nuevoPedido.setCliente(cliente); ver como recuperar el cliente
+		
+		try{
+		controladorPedido.AddPedido(nuevoPedido);
+		response.sendRedirect("/ListaPedidos.jsp");/*Redirige a la lista completa de Pedidos*/
+		}
+		catch(Exception e)
+		{
+			response.sendRedirect("/ErrorAlta.html"); //No está creada esta vista1.
+		}
+		// TODO Auto-generated method stub
+	}
+
+	
+}
